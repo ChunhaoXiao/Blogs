7de095a8ac101306335ed00dc1130b64d8411cc0
vagrant <vagrant@homestead>
2018-02-02 06:14:33 +0000

    modify migration

--- database/migrations/2018_01_02_073531_create_thumbs.php
+++ database/migrations/2018_01_02_073531_create_thumbs.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Create_Thumbs extends Migration
+class CreateThumbs extends Migration
 {
     /**
      * Run the migrations.
